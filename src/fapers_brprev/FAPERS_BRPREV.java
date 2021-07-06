32c5f41226b13cfecc39e7e6f82d88cd3bc0aa5a
Luan <Luan@DESKTOP-B4ERJ5H>
2021-07-06 12:30:34 -0300

    Added create map of accounts and hp in main class

--- src/fapers_brprev/FAPERS_BRPREV.java
+++ src/fapers_brprev/FAPERS_BRPREV.java
@@ -21,51 +21,50 @@ public class FAPERS_BRPREV {
         try {
 
             //Arquivo de contas
-            File accountsFile = new File("./FAPERS_Accounts.csv");
+            File accountsFile = FileManager.getFile("./FAPERS_Accounts.csv");
+            File hpFile = FileManager.getFile("./FAPERS_HP.csv");
 
-            if (accountsFile.exists()) {
-                //Cria mapa de filtros de contas e historicos padroes
-                Accounts.addOnList(accountsFile);
+            //Cria mapa de filtros de contas e historicos padroes
+            Accounts.createAccountsMap(accountsFile);
+            Accounts.createHpMap(hpFile);
 
-                //Pega mes e ano
-                month = Integer.valueOf(JOptionPane.showInputDialog("Insira o mês:"));
-                year = Integer.valueOf(JOptionPane.showInputDialog("Insira o ano:"));
+            //Pega mes e ano
+            month = Integer.valueOf(JOptionPane.showInputDialog("Insira o mês:"));
+            year = Integer.valueOf(JOptionPane.showInputDialog("Insira o ano:"));
 
-                //Mapa com importações em String para fazer
-                List<Map<String, String>> imports = new ArrayList<>();
+            //Mapa com importações em String para fazer
+            List<Map<String, String>> imports = new ArrayList<>();
 
-                //Pega lctos para importyação do banco
-                imports.addAll(PayRoll.getImports());
+            //Pega lctos para importyação do banco
+            imports.addAll(PayRoll.getImports());
 
-                //Salva o arquivo de texto para importação no formato do Layout correto
-                File desktopPath = new File(System.getProperty("user.home") + "/Desktop/");
-                if (FileManager.save(
-                        desktopPath,
-                        "FAPERS_BVPREV_import.csv",
-                        Layout.getLayoutOfMaps(imports)
-                )) {
-                    if (!"".equals(log.toString())) {
-                        FileManager.save(desktopPath, "LOG_FAPERS.csv", log.toString());
-                        JOptionPane.showMessageDialog(
-                                null,
-                                "Alguns lançamentos não foram para o arquivo de layout porque não foram encontrados no arquivo de contas."
-                                + "\nSalvei um log na sua area de trabalho informando quais não foram importados."
-                                + "\nEstou abrindo para você completar o arquivo de contas."
-                                + "\nPreencha o campo de histórico com os termos que o historico deve ter separados por espaços."
-                                + "\nPreencha com pelo menos uma conta de debito ou credito."
-                        );
+            //Salva o arquivo de texto para importação no formato do Layout correto
+            File desktopPath = new File(System.getProperty("user.home") + "/Desktop/");
+            if (FileManager.save(
+                    desktopPath,
+                    "FAPERS_BVPREV_import.csv",
+                    Layout.getLayoutOfMaps(imports)
+            )) {
+                if (!"".equals(log.toString())) {
+                    FileManager.save(desktopPath, "LOG_FAPERS.csv", log.toString());
+                    JOptionPane.showMessageDialog(
+                            null,
+                            "Alguns lançamentos não foram para o arquivo de layout porque não foram encontrados debito, credito ou historico nos arquivos de DE_PARA."
+                            + "\nSalvei um log na sua area de trabalho informando quais não foram importados."
+                            + "\nEstou abrindo para você completar o arquivo de contas e de historicos."
+                            + "\nPreencha o campo de histórico com os termos que o historico deve ter separados por espaços."
+                            + "\nPreencha as contas da FAPERS e a correspondente do UNICO, o nome da conta não é obrigatório, é apenas para identificação."
+                    );
 
-                        //Abre o arquivo de contas para a pessoa completar
-                        Desktop.getDesktop().open(accountsFile);
-                    }
+                    //Abre o arquivo de contas para a pessoa completar
+                    Desktop.getDesktop().open(accountsFile);
+                    Desktop.getDesktop().open(hpFile);
+                }
 
-                    JOptionPane.showMessageDialog(null, "Arquivo layout de importação salvo na área de trabalho");
-                } else {
-                    JOptionPane.showMessageDialog(null, "Erro ao salvar o arquivo final!");
-                };
+                JOptionPane.showMessageDialog(null, "Arquivo layout de importação salvo na área de trabalho");
             } else {
-                throw new Exception("O arquivo 'FAPERS_Accounts.csv' não existe na pasta do programa!");
-            }
+                JOptionPane.showMessageDialog(null, "Erro ao salvar o arquivo final!");
+            };
         } catch (Exception e) {
             e.printStackTrace();
             JOptionPane.showMessageDialog(null, "Erro: " + e.getMessage(), "Erro!", JOptionPane.ERROR_MESSAGE);
