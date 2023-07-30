911260d267c03a8e602409618e4cd301e6ab9940
luangrabher <luanggcontato@gmail.com>
2023-07-30 18:51:49 -0300

    delete old files netbeans

--- sql/get_FAPERS_monthEntries.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-BDCOMPL as historico,
-BDDEBITO as debito,
-BDCREDITO as credito,
-BDVALOR as valor
-FROM VSUC_EMPRESAS_TLAN L
-WHERE
-BDCODEMP = :enterprise
-AND BDDATA >= ':year-:month-01' AND BDDATA <= ':year-:month-:lastDayOfMonth'
